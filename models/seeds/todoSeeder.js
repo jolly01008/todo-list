b405baed5d0b944579a14b5eb549c6ce37c52a95
jolly01008 <jollgmll292@gmail.com>
2023-03-07 22:59:13 +0800

    feat: add todo seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,17 +1,13 @@
-//載入express框架
-const express = require("express");
 //載入mongoose
 const mongoose = require("mongoose");
-//設定連接埠
-const port = 3000;
+
+//載入todo model
+const Todo = require("../todo");
 
 //加入這段code，僅在非正式環境時，使用dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-//啟動Express應用程式伺服器，設定app代表伺服器
-const app = express();
 //設定連線到mongoDB。程式跑到這行時，會與資料庫連線
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -26,14 +22,10 @@ db.on("error", () => {
 
 db.once("open", () => {
   console.log("mongodb connected!");
-});
 
-//設定路由
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` });
+  }
 
-//伺服器監聽 port 3000
-app.listen(port, () => {
-  console.log("Run todo-list Project http://localhost:3000");
+  console.log("done.");
 });
