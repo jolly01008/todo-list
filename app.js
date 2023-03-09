b8fa4fecf1803059e26de53a56ffccc7dc723b84
jolly01008 <jollgmll292@gmail.com>
2023-03-09 22:57:38 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 //設定連接埠
 const port = 3000;
-
 //載入handlebars
 const exphbs = require("express-handlebars");
 
 //啟動Express應用程式伺服器，設定app代表伺服器
 const app = express();
+
+//載入TodoModel
+const Todo = require("./models/todo");
 
 //加入這段code，僅在非正式環境時，使用dotenv
 if (process.env.NODE_ENV !== "production") {
@@ -35,9 +37,13 @@ db.once("open", () => {
 //載入handlebars，把樣板引擎指定為Handlebars
 app.engine("hbs", exphbs({ defaultLayouts: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-//設定路由
+
+//設定路由;
 app.get("/", (req, res) => {
-  res.render("index");
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)); // 錯誤處理
 });
 
 //伺服器監聽 port 3000
