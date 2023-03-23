d173cf8ff27b45a3ac7206fed6bf9fbcc86094ab
jolly01008 <jollgmll292@gmail.com>
2023-03-24 00:34:26 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.set("view engine", "hbs");
 //==========設定index路由==========;
 app.get("/", (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
+    .sort({_id : "asc"}) //mongoose提供的排序方法。也可用desc
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error)); // 錯誤處理
