4a2c11fd39c9d4ebd2ce373feef0e01ea5cf7f76
jolly01008 <jollgmll292@gmail.com>
2023-03-30 00:01:59 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('./config/mongoose')
 const routes = require('./routes')  // 引用路由器
 
 const app = express() //啟動Express應用程式伺服器，設定app代表伺服器
+const PORT = process.env.PORT || 3000 // 如果在 Heroku 環境則使用 process.否則為本地環境，使用 3000 env.PORT
 
 //載入handlebars，把樣板引擎指定為Handlebars
 app.engine("hbs", exphbs({ defaultLayouts: "main", extname: ".hbs" }))
@@ -19,6 +20,6 @@ app.use(methodOverride('_method'))
 app.use(routes)  // 將 request 導入路由器
 
 //伺服器監聽 port 3000
-app.listen(port, () => {
-  console.log("Run todo-list Project http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Run todo-list Project http://localhost:${PORT}`);
 });
