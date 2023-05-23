5234eafd3b716926d4725dc7157ae653e6a8417f
jolly01008 <jollgmll292@gmail.com>
2023-05-23 22:02:00 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const port = 3000 //設定連接埠
 const exphbs = require("express-handlebars") //載入handlebars
 const bodyParser = require("body-parser") //引用pody-parser
 const methodOverride = require("method-override")  //載入method-override
-const session = require("express-session")  //載入xpress-session
+const session = require("express-session")  //載入express-session
 
 require('./config/mongoose')
 const routes = require('./routes')  // 引用路由器
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 
 const app = express() //啟動Express應用程式伺服器，設定app代表伺服器
 const PORT = process.env.PORT || 3000 // 如果在 Heroku 環境則使用 process.否則為本地環境，使用 3000 env.PORT
@@ -23,6 +24,8 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true })) //設定bodyParser
 app.use(methodOverride('_method'))
+
+usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
 app.use(routes)  // 將 request 導入路由器
 
