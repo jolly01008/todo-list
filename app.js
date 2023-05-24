9f65d660e002ec1fd04e353f0fb3cf239be6a63c
jolly01008 <jollgmll292@gmail.com>
2023-05-24 21:50:06 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use(methodOverride('_method'))
 
 usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
+app.use((req , res , next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)  // 將 request 導入路由器
 
 //伺服器監聽 port 3000
