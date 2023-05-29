7671f6a8fb949428a8523e63dcaf5a24275beefa
jolly01008 <jollgmll292@gmail.com>
2023-05-30 00:10:57 +0800

    modify todoSeeder & fix delete button & add .evn.example

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,36 @@
+const bcrypt = require("bcryptjs") 
 
+
+const User = require("../user") //載入user model
 const Todo = require("../todo") //載入todo model
 const db = require('../../config/mongoose') //引入mongooseDB連線設定
 
-db.once("open", () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` });
-  }
+const SEED_USER = {
+  name : "root",
+  email: "root@example.com",
+  password: "12345678",
+}
 
-  console.log("done.");
-});
+db.once("open", () => {
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password , salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email:  SEED_USER.email,
+      password: hash 
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (value, i) => 
+        Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+})
+
