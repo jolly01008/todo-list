7ea722bedc11098c52bb978207206e67ddf5a1f1
jolly01008 <jollgmll292@gmail.com>
2023-06-07 21:49:21 +0800

    modify flash message at login page

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@ const passport = require("passport") //載入passport
 const LocalStrategy = require("passport-local").Strategy
 const bcrypt = require("bcryptjs") //引用bcryptjs套件
 const FacebookStrategy = require("passport-facebook").Strategy   //引入facebook strategy 
-
 const User = require('../models/user')  //載入 User model
 
 module.exports = app =>{
@@ -10,16 +9,16 @@ module.exports = app =>{
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email'} , (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email' ,passReqToCallback: true } , (req , email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!'})
+          return done(null, false, req.flash('warning_msg' , '這個信箱還沒註冊!'))
         }
         return bcrypt.compare(password, user.password)
           .then(isMatch => {
             if(!isMatch){
-              return done(null , false ,{message: 'Email or Password incorrect.'})
+              return done(null , false ,req.flash('warning_msg' , '信箱或密碼不正確!'))
             }
           return done(null , user)
           })
